c25ec2071498743b91a6b2869f123fbe8f85d0f9
mohaa24 <msmathnan@gmail.com>
2022-07-19 17:49:28 +0530

    ...

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  let count = 0;
+  setInterval(() => {
+    count++;
+  }, 1000);
+
+  res.status(200).json({'count':count})
 }
