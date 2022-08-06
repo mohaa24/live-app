d92eb92fc7ef2479f7fce201ddf3483ff4cdaf3d
mohaa24 <msmathnan@gmail.com>
2022-08-06 20:38:19 +0530

    ..

--- pages/blog.js
+++ pages/blog.js
@@ -2,70 +2,67 @@ import { MapComp } from "../components/map/map";
 import { Carousel } from "react-responsive-carousel";
 import styled from "styled-components";
 import { Appraisal } from "../components/appraisal/appraisal";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // import andersonPark from "../public/andersonPark";
 const AboutSlide = styled.div`
   height: 100vh;
 `;
 
 export default function Contact() {
-  const imageSlides = [1].map((i) => {
+    const [response, setResponse] = useState(null);
+    
+
+ 
+    let apiURL = "http://localhost:3100/posts";
+    // apiURL = "https://fluffy-garbanzo.herokuapp.com/posts";
+
+    useEffect(() => {
+      requestBlogData();
+    }, []);
+
+  
+
+    const requestBlogData = async () => {
+      axios({
+        method: "get",
+        url: apiURL,
+        headers: {
+          //  Authorization: `Bearer ${token}`,
+          //  "X-Api-Key": key
+        },
+      })
+        .then((response) => {
+          //console.log(response.data.data.items);
+          setResponse(response.data);
+          console.log(response.data, "responseee");
+        })
+        .catch((error) => console.log(error));
+    };
+
+
+  let cards = response && response.map((i)=>{
     return (
-      <AboutSlide
-        className="propertySlides"
-        style={{
-          background: `url(/andersonPark.jpg)`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {/* <img
-          className="propertImage"
-          src={i.url}
-          alt="1403/14 George Avenue Broadbeach"
-        /> */}
-      </AboutSlide>
+      <a class="card" href= {`/post?id=${i._id}`} >
+        <img
+          class="card__background"
+          src={i.img}
+        />
+        <div class="card__text-container">
+          <p class="card__category">{i.type}</p>
+          <p class="card__title">{i.title}</p>
+        </div>
+      </a>
     );
-  });
+  })
   return (
     <>
-      {/* <section className="aboutSection">
-        <div className="aboutIntro">
-          <h1>ABOUT US</h1>
-
-          <h3>
-            A CORE BUSINESS MODEL BASED ON RELATIONSHIPS, EXCELLENCE AND
-            AUTHENTICITY PREVAIL TO ENSURE A CUSTOMER EXPERIENCE OF THE HIGHEST
-            STANDARD.
-          </h3>
-        </div>
-        {imageSlides && imageSlides}
-      </section> */}
+     
       <section className="blogSection">
         <h1>Blog</h1>
 
         <div className="news_wrapper">
-          <a class="card" href="/post">
-            <img
-              class="card__background"
-              src="https://kollosche-1bfb7.kxcdn.com/wp-content/uploads/2022/08/165777114369571257-rsd.jpeg"
-            />
-            <div class="card__text-container">
-              <p class="card__category">Post</p>
-              <p class="card__title">The Best Suburb for Investors.</p>
-            </div>
-          </a>
-          <a class="card" href="/post">
-            <img
-              class="card__background"
-              src="https://kollosche-1bfb7.kxcdn.com/wp-content/uploads/2022/07/Feature-Image-scaled.jpg"
-            />
-            <div class="card__text-container">
-              <p class="card__category">Article</p>
-              <p class="card__title">
-                The Gold Coast’s Newest Million Dollar Suburbs.
-              </p>
-            </div>
-          </a>
+        {cards && cards}
         </div>
       </section>
     </>
--- pages/post.js
+++ pages/post.js
@@ -1,101 +1,88 @@
-import { MapComp } from "../components/map/map";
-import { Carousel } from "react-responsive-carousel";
-import styled from "styled-components";
-import { Appraisal } from "../components/appraisal/appraisal";
-// import andersonPark from "../public/andersonPark";
-const AboutSlide = styled.div`
-  height: 100vh;
-`;
+
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import OverlayLoader from "../components/loading/loading";
+
 
 export default function Contact() {
-  const imageSlides = [1].map((i) => {
-    return (
-      <AboutSlide
-        className="propertySlides"
-        style={{
-          background: `url(/andersonPark.jpg)`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {/* <img
-          className="propertImage"
-          src={i.url}
-          alt="1403/14 George Avenue Broadbeach"
-        /> */}
-      </AboutSlide>
-    );
+      const [response, setResponse] = useState(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router.query.id);
+    requestPostData()
   });
+  let apiURL = "http://localhost:3100/getPost";
+  // apiURL = "https://fluffy-garbanzo.herokuapp.com/posts";
+
+  const requestPostData = async () => {
+    axios({
+      method: "get",
+      url:`${apiURL}?id=${router.query.id}`,
+      headers: {
+        //  Authorization: `Bearer ${token}`,
+        //  "X-Api-Key": key
+      },
+    })
+      .then((response) => {
+        //console.log(response.data.data.items);
+        setResponse(response.data[0]);
+        console.log(response.data, "responseee");
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
-      {/* <section className="aboutSection">
-        <div className="aboutIntro">
-          <h1>ABOUT US</h1>
+      {!response && <OverlayLoader />}
 
-          <h3>
-            A CORE BUSINESS MODEL BASED ON RELATIONSHIPS, EXCELLENCE AND
-            AUTHENTICITY PREVAIL TO ENSURE A CUSTOMER EXPERIENCE OF THE HIGHEST
-            STANDARD.
-          </h3>
-        </div>
-        {imageSlides && imageSlides}
-      </section> */}
-      <section className="postSection">
-        <h1>Post title comes here...</h1>
+      {response && (
+        <section className="postSection">
+          <h1>{response.title}</h1>
 
-        <div className="postContent">
-          <div className="postMeta">
-            <div className="authorDetails">
-              Steven Dick, Commercial Partner, A__Z
+          <div className="postContent">
+            <div className="postMeta">
+              <div className="authorDetails">
+                Steven Dick, Commercial Partner, A__Z
+              </div>
+              <div className="postedTime">April 2022</div>
             </div>
-            <div className="postedTime">April 2022</div>
-          </div>
-          <hr/>
+            <hr />
 
-          <p>
-            <img
-              src="https://images.unsplash.com/photo-1601168049797-351a4abb0b2f?crop=entropy&amp;cs=srgb&amp;fm=jpg&amp;ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY1MDY1NDk3Nw&amp;ixlib=rb-1.2.1&amp;q=85"
-              alt="a green hummingbird flying among yellow flowers"
-              title="Photo by Geronimo Giqueaux for Unsplash"
-              class="editorial-image-right"
-            ></img>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            imperdiet ut quam sit amet vehicula. Donec sit amet facilisis quam.
-            Integer mollis, urna accumsan tempor hendrerit, risus neque
-            tincidunt neque, in aliquam elit eros quis tortor. Sed id venenatis
-            massa, ut malesuada sem. Nam lacinia sodales tellus nec efficitur.
-            Vestibulum fringilla nisl ac iaculis ultricies. Sed commodo
-            imperdiet metus vitae molestie. In laoreet rutrum pretium. Aenean a
-            enim ac lacus tincidunt pellentesque ac a tellus. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Vivamus imperdiet ut quam sit
-            amet vehicula. Donec sit amet facilisis quam. Integer mollis, urna
-            accumsan tempor hendrerit, risus neque tincidunt neque, in aliquam
-            elit eros quis tortor. Sed id venenatis massa, ut malesuada sem. Nam
-            lacinia sodales tellus nec efficitur. Vestibulum fringilla nisl ac
-            iaculis ultricies.
-            <br />
-            <br />
-            Sed commodo imperdiet metus vitae molestie. In laoreet rutrum
-            pretium. Aenean a enim ac lacus tincidunt pellentesque ac a tellus.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            imperdiet ut quam sit amet vehicula. Donec sit amet facilisis quam.
-            Integer mollis, urna accumsan tempor hendrerit, risus neque
-            tincidunt neque, in aliquam elit eros quis tortor. Sed id venenatis
-            massa, ut malesuada sem. Nam lacinia sodales tellus nec efficitur.
-            Vestibulum fringilla nisl ac iaculis ultricies. Sed commodo
-            imperdiet metus vitae molestie. In laoreet rutrum pretium. Aenean a
-            enim ac lacus tincidunt pellentesque ac a tellus.Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Vivamus imperdiet ut quam sit
-            amet vehicula. Donec sit amet facilisis quam. Integer mollis, urna
-            accumsan tempor hendrerit, risus neque tincidunt neque, in aliquam
-            elit eros quis tortor. Sed id venenatis massa, ut malesuada sem. Nam
-            lacinia sodales tellus nec efficitur. Vestibulum fringilla nisl ac
-            iaculis ultricies. Sed commodo imperdiet metus vitae molestie. In
-            laoreet rutrum pretium. Aenean a enim ac lacus tincidunt
-            pellentesque ac a tellus.
-          </p>
-        </div>
-      </section>
+            <p>
+              <img
+                src={response.img}
+                alt="a green hummingbird flying among yellow flowers"
+                title="Photo by Geronimo Giqueaux for Unsplash"
+                class="editorial-image-right"
+              ></img>
+              {response.content}
+              <br />
+              <br />
+              Sed commodo imperdiet metus vitae molestie. In laoreet rutrum
+              pretium. Aenean a enim ac lacus tincidunt pellentesque ac a
+              tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vivamus imperdiet ut quam sit amet vehicula. Donec sit amet
+              facilisis quam. Integer mollis, urna accumsan tempor hendrerit,
+              risus neque tincidunt neque, in aliquam elit eros quis tortor. Sed
+              id venenatis massa, ut malesuada sem. Nam lacinia sodales tellus
+              nec efficitur. Vestibulum fringilla nisl ac iaculis ultricies. Sed
+              commodo imperdiet metus vitae molestie. In laoreet rutrum pretium.
+              Aenean a enim ac lacus tincidunt pellentesque ac a tellus.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              imperdiet ut quam sit amet vehicula. Donec sit amet facilisis
+              quam. Integer mollis, urna accumsan tempor hendrerit, risus neque
+              tincidunt neque, in aliquam elit eros quis tortor. Sed id
+              venenatis massa, ut malesuada sem. Nam lacinia sodales tellus nec
+              efficitur. Vestibulum fringilla nisl ac iaculis ultricies. Sed
+              commodo imperdiet metus vitae molestie. In laoreet rutrum pretium.
+              Aenean a enim ac lacus tincidunt pellentesque ac a tellus.
+            </p>
+          </div>
+        </section>
+      )}
     </>
   );
 }
